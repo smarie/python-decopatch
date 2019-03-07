16be9bc5a73ee055014ceb5d24afebb3f01c3e52
Sylvain MARIE <sylvain.marie@se.com>
2019-03-07 10:54:05 +0100

    updated keywords, they were wrong!

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ LICENSE_LONG = 'License :: OSI Approved :: BSD License'
 version_for_download_url = get_version()
 DOWNLOAD_URL = URL + '/tarball/' + version_for_download_url
 
-KEYWORDS = 'pytest test result store fixture collect benchmark artifact session'
+KEYWORDS = 'decorator parenthesis function class auto easy signature preserve'
 # --Get the long description from the README file
 # with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 #    LONG_DESCRIPTION = f.read()
