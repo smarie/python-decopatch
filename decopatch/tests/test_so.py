28ae15eb297c8f24b12a464a6282230ffd6ca627
Sylvain MARIE <sylvain.marie@se.com>
2019-03-11 16:31:01 +0100

    Added tests for stack overflow answer

--- decopatch/tests/test_so.py
+++ decopatch/tests/test_so.py
@@ -1,10 +1,12 @@
 from __future__ import print_function
 
+import pytest
+
 from decopatch import function_decorator, WRAPPED, F_ARGS, F_KWARGS, DECORATED
 from makefun import wraps
 
 
-def test_so0():
+def test_so_flat():
     """
     Tests that answer at
     https://stackoverflow.com/questions/28693930/when-to-use-decorator-and-decorator-factory/55078327#55078327 is
@@ -29,7 +31,7 @@ def test_so0():
     assert running_func(1, 2) == 102
 
 
-def test_so1():
+def test_so_double_flat():
     """
     Tests that answer at
     https://stackoverflow.com/questions/28693930/when-to-use-decorator-and-decorator-factory/55078327#55078327 is
@@ -49,3 +51,41 @@ def test_so1():
 
     assert running_func(-1, 2) == 1
     assert running_func(1, 2) == 102
+
+
+@pytest.mark.parametrize('style', ['flat', 'double-flat'], ids="style={}".format)
+def test_so_wrap(style):
+    """
+    Tests that the answer at
+    https://stackoverflow.com/a/55105198/7262247
+    works correctly
+    :return:
+    """
+    if style == 'flat':
+        @function_decorator
+        def makestyle(style='b', fn=DECORATED):
+            open_tag = "<%s>" % style
+            close_tag = "</%s>" % style
+
+            @wraps(fn)
+            def wrapped(*args, **kwargs):
+                return open_tag + fn(*args, **kwargs) + close_tag
+
+            return wrapped
+    else:
+        @function_decorator
+        def makestyle(style='b', fn=WRAPPED, f_args=F_ARGS, f_kwargs=F_KWARGS):
+            open_tag = "<%s>" % style
+            close_tag = "</%s>" % style
+            return open_tag + fn(*f_args, **f_kwargs) + close_tag
+
+    @makestyle
+    @makestyle('i')
+    def hello(who):
+        return "hello %s" % who
+
+    assert hello('world') == '<b><i>hello world</i></b>'
+
+    # you can check that
+    with pytest.raises(TypeError):
+        hello()  # TypeError: hello() missing 1 required positional argument: 'who'
