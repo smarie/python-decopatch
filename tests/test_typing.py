d5762b50ceb0924fcc2eab94921eb0bac5ef30b7
Serg Tereshchenko <serg.partizan@gmail.com>
2022-02-11 21:15:21 +0200

    chore: Fix tests

--- tests/test_typing.py
+++ tests/test_typing.py
@@ -5,25 +5,28 @@ from decopatch import DECORATED, function_decorator
 import pytest
 
 
-@pytest.mark.xfail(raises=TypeError)
 def test_invalid_parameter():
-    # Error, invalid argument
-    @function_decorator(invalid_param=True)
-    def decorator_wint_invalid_param():
-        pass
+    with pytest.raises(TypeError):
+        # Error, invalid argument
+        @function_decorator(invalid_param=True)
+        def decorator_wint_invalid_param(fn=DECORATED):
+            return fn
 
 
 def test_normal_decorator():
     @function_decorator
-    def decorator(scope: str = "test", f=DECORATED):
-        pass
+    def decorator(scope: str = "test", fn=DECORATED):
+        assert isinstance(scope, str)
+        return fn
 
     # Ok
     @decorator
     def decorated_flat():
         pass
 
-    with pytest.raises(TypeError):
+    assert decorated_flat
+
+    with pytest.raises(AssertionError):
         # Error, Literal[2] is incompatible with str
         @decorator(scope=2)
         def decorated_with_invalid_options():
@@ -34,14 +37,18 @@ def test_normal_decorator():
     def decorated_with_valid_options():
         pass
 
+    assert decorated_with_valid_options
 
-def test_decorator_with_params():
+
+def test_function_decorator_with_params():
     # Ok, should reveal correct type for `enable_stack_introspection`
     @function_decorator(enable_stack_introspection=True)
-    def decorator_with_params(scope: str = "test", f=DECORATED):
-        pass
+    def decorator_with_params(scope: str = "test", fn=DECORATED):
+        return fn
 
     # Ok, should reveal correct type for `scope`
     @decorator_with_params(scope="success")
-    def decorated_with_valid_options_v2():
+    def decorated_with_valid_options():
         pass
+
+    assert decorated_with_valid_options
